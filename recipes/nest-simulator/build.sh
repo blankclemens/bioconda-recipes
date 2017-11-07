0f120771b8d9f498d0d38da7d7fc035b617a0587
Clemens Blank <blankclemens@gmail.com>
2017-11-07 14:59:33 +0000

    Cleanup

--- recipes/nest-simulator/build.sh
+++ recipes/nest-simulator/build.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
 
-# export CFLAGS="-I$PREFIX/include"
-# export LDFLAGS="-L$PREFIX/lib"
-
-# ./bootstrap.sh
-# ./configure --prefix=$PREFIX              \
-#             --with-pynest-prefix=$PREFIX  \
-#             --without-openmp              \
-#             --without-readline
-
-# make
-# make install
-
 cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX -Dwith-mpi=ON -Dwith-openmp=ON -Dwith-includes=$PREFIX/include -Dwith-libraries=$PREFIX/lib ./
-make -j 8
+make -j 2
 make install
